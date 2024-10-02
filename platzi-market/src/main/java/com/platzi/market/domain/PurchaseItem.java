d3b7690f16f5e911020fae068bfc19c7fd3cc11e
Jose Francisco Ortega <67157652+joseortega-hub@users.noreply.github.com>
2024-10-02 18:03:14 +0200

    primerVersionApi/Spring

---
+++ platzi-market/src/main/java/com/platzi/market/domain/PurchaseItem.java
@@ -0,0 +1,40 @@
+package com.platzi.market.domain;
+
+public class PurchaseItem {
+    private int productId;
+    private int quantify;
+    private double total;
+    private boolean active;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantify() {
+        return quantify;
+    }
+
+    public void setQuantify(int quantify) {
+        this.quantify = quantify;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
